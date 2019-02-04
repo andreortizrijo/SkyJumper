133ebfdf3e996ff0300fa81775f8382426236ca3
André Alexandre Ortiz Rijo <andrerijo@Andres-MacBook-Pro.local>
2019-02-04 15:07:00 +0000

    Start jumping

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform player;
+    private float camSpeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (player)
+        {
+            transform.position = Vector3.Lerp(transform.position, player.position, camSpeed) + new Vector3(0, 0, -10);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float jumForce = 7;
+    public Rigidbody rb;
+    public BoxCollider col;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<BoxCollider>();
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            rb.AddForce(Vector3.up * jumForce, ForceMode.Impulse);
+        }
+    }
+}
