2973d7e510903cfe78750c2a0fe83c31864da4e4
Tomas1207 <tomasfernandes98@gmail.com>
2019-02-04 16:53:53 +0000

    Spawner basic

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject plat;
+    public Transform[] spawnPoints;
+
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(spawnPalt());
+	}
+	
+	IEnumerator spawnPalt()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+
+
+            int spawnIndex = Random.Range(0, spawnPoints.Length);
+            Transform spawnPoint = spawnPoints[spawnIndex];
+
+            Instantiate(plat, spawnPoint.position, spawnPoint.rotation);
+
+
+        }
+    }
+}
