7c84ca87e72f66012a943d25b2182355c23aab44
Tomas1207 <tomasfernandes98@gmail.com>
2019-03-01 20:02:01 +0000

    Spawn Bluit

--- Assets/Scripts/Plataform_Spawn.cs
+++ Assets/Scripts/Plataform_Spawn.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Plataform_Spawn : MonoBehaviour
 {
+    public Camera Cam = new Camera();
+    public bool isLive = true;
+    public float maxwidth;
+    public GameObject[] objecto;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,23 +18,23 @@ public class Plataform_Spawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 UpperC = new Vector3(Screen.width, Screen.height, 0.0f);
+        Vector3 largura = Cam.ScreenToWorldPoint(UpperC);
+        maxwidth = largura.x - objecto[0].GetComponent<Renderer>().bounds.extents.x;
+        StartCoroutine(Spawn());
     }
 
     IEnumerator Spawn()
     {
         yield return new WaitForSeconds(2.0f);
-        GameObject bola;
-        while (timeleft > 0)
+        
+        while (isLive)
         {
-            timerText.text = "Timer Left: \n" + timeleft;
             Vector3 spawnposition = new Vector3(Random.Range(-maxwidth, maxwidth), transform.position.y, 0.0f);
             Instantiate(objecto[Random.Range(0, objecto.Length)], spawnposition, Quaternion.identity);
-            yield return new WaitForSeconds(2.0f);
+            yield return new WaitForSeconds(5.0f);
         }
-        yield return new WaitForSeconds(2.0f);
-        Gameover.SetActive(true);
-        yield return new WaitForSeconds(1.0f);
-        btnRestart.SetActive(true);
+     
+        
     }
 }
